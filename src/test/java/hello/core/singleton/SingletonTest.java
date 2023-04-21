8367fa98ad97abb3f2d742c6d82560f0de0ac45a
Jealyang <jaelyangchoi@gmail.com>
2023-04-21 23:24:06 +0900

    싱글톤 컨테이너 스프링 컨테이너는 싱글턴 패턴을 적용하지 않아도, 객체 인스턴스를 싱글톤으로 관리한다

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -19,9 +19,10 @@ class SingletonTest {
         //1. 조회 : 호출할 때마다 객체를 생성
         MemberService memberService1 = appConfig.memberService();
         MemberService memberService2 = appConfig.memberService();
+
+        //2. 확인 : 객체가 서로 다름
         System.out.println("memberService1 = " + memberService1);
         System.out.println("memberService2 = " + memberService2);
-
         Assertions.assertThat(memberService1).isNotSameAs(memberService2);
     }
 
@@ -31,16 +32,37 @@ class SingletonTest {
         //private으로 생성자를 막아두었다. 컴파일 오류가 발생한다.
         //SingletonService singletonService = new SingletonService();
 
+        //1. 조회 : 호출할 때마다 싱글톤 객체를 반환
         SingletonService singletonService1 = SingletonService.getInstance();
         SingletonService singletonService2 = SingletonService.getInstance();
-
-        System.out.println("singletonService1 = " + singletonService1);
-        System.out.println("singletonService2 = " + singletonService2);
         singletonService1.logic();
 
-        //참조값이 같은 것을 확인
+        //2. 확인 : 객체가 서로 같음 (참조값이 같은 것을 확인)
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
         Assertions.assertThat(singletonService1).isSameAs(singletonService2);
         //isSameAS : == (참조 비교)
         //isEqualTo : 자바 equals 메소드
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너는 싱글턴 패턴을 적용하지 않아도, 객체 인스턴스를 싱글톤으로 관리한다.")
+    void springContainer(){
+        //AppConfig appConfig = new AppConfig(); //pureContainer
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //1. 조회 : 호출할 때마다 싱글톤 객체를 반환
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        //2. 확인 : 객체가 서로 같음 (참조값이 같은 것을 확인)
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
+
+        /* 스프링 컨테이너의 이런 기능 덕분에 싱글턴 패턴의 모든 단점을 해결하면서 객체를 싱글톤으로 유지할 수 있다.
+            - 싱글톤 패턴을 위한 지저분한 코드가 들어가지 않아도 된다.
+            - DIP, OCP, 테스트, private 생성자로 부터 자유롭게 싱글톤을 사용할 수 있다.
+         */
     }
 }
