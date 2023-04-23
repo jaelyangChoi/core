689e786859052d5dec3307943e0974c596382cd5
Jealyang <jaelyangchoi@gmail.com>
2023-04-23 19:20:26 +0900

    탐색 위치와 기본 스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,8 +6,10 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration //싱글톤 빈을 보장
 @ComponentScan( //@Component가 붙은 클래스를 전부 스캔해 빈으로 등록
-        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+        //탐색 대상 지정
+//        basePackages = {"hello.core"}, //default는 @ComponentScan이 붙은 설정 정보 클래스의 패키지.
         //기존 설정 정보를 빈으로 등록하지 않기 위해.
+        excludeFilters= @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
 }
--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -3,6 +3,7 @@ package hello.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@ComponentScan이 포함되어 있어 스프링 부트 애플리케이션일 경우 따로 @ComponentScan을 붙일 필요가 없다.
 @SpringBootApplication
 public class CoreApplication {
 
