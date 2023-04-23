5a3fdd8a1b498cdb8fab15699224e9a49173aa50
Jealyang <jaelyangchoi@gmail.com>
2023-04-23 19:38:34 +0900

    ComponentScan 필터

--- src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
+++ src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -1,6 +1,5 @@
 package hello.core.scan.filter;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.ApplicationContext;
@@ -8,8 +7,6 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
-
-import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
